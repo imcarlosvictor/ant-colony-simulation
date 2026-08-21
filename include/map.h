f52befb0d77ebfa00ff480f1dc1e0d7396108d39
Lucas <imcvlucas@gmail.com>
2026-08-20 23:19:48 -0400

    populated map.cpp

--- include/map.h
+++ include/map.h
@@ -4,15 +4,12 @@
 
 class Map {
 	public:
-		Map();
 		Map(int width, int height);
-		~Map();
-		void destroy();
 		void createMap();
 		int getWidth();
 		int getHeight();
 
 	private:
 		int width;
-		int height;
+		int length;
 };
---
+++ src/map.cpp
@@ -0,0 +1,21 @@
+#include "../include/map.h"
+
+
+Map::Map(int width, int length) {
+	width = this->width;
+	length = this->length;
+}
+
+void Map::createMap() {
+	int x = 0;
+	int y = 0;
+}
+
+int Map::getWidth() {
+	return this->width;
+}
+
+int Map::getHeight() {
+
+return this->length;
+}
